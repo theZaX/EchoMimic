e3b4beb8f50e3bf3dac56ad4b1b57a2c7789e127
Zachary Allen <zack@mindsmith.ai>
2024-07-21 08:23:35 +0000

    added install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install requirements from requirements.txt
+pip install -r requirements.txt
+
+# Install libgl1-mesa-glx
+sudo apt-get update && sudo apt-get install -y libgl1-mesa-glx
+
+# Install git-lfs
+sudo apt-get update && sudo apt-get install -y git-lfs
+
+# Initialize git-lfs
+git lfs install
+
+# Clone pretrained weights from Hugging Face
+git clone https://huggingface.co/BadToBest/EchoMimic pretrained_weights
+
+# Install ffmpeg
+wget https://www.johnvansickle.com/ffmpeg/old-releases/ffmpeg-4.4-amd64-static.tar.xz
+tar xvJf ffmpeg-4.4-amd64-static.tar.xz
+
+# Set FFMPEG_PATH environment variable
+export FFMPEG_PATH=$(pwd)/ffmpeg-4.4-amd64-static
+
+echo "Installation complete and FFMPEG_PATH set to $FFMPEG_PATH"
